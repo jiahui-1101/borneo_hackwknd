921eea4dce2815ebe6ac315854155df7cb402571
thungthungg <thungthung2005@gmail.com>
2026-03-05 17:50:12 +0800

    addition

--- my_huat/lib/features/insurance/pages/insurance_page.dart
+++ my_huat/lib/features/insurance/pages/insurance_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'tutorial_page.dart';
 import 'my_goals_page.dart';
 
 class InsurancePage extends StatefulWidget {
